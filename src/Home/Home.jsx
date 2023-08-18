fbba879ea14ade5f8df1f3ded7d721f49cce20c3
ThaissaMariana <thaissa2608mariana@gmail.com>
2023-08-18 14:16:21 -0400

    commit alterar

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function Home() {
-
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
     return (
         <div>
           <h1>Home</h1>
-          <Link to="/Todo">TODO</Link>
+          <Link to="/Todo">TO DO LIST</Link>
+          <p>{count}</p>
+          <p>{name}</p>
+          <button onClick={() => { setCount(count + 1) }}></button>
+          <button onClick={() => { setCount(0) }}>Reset</button>
+          <input type="text" onChange={(e)=>{setName(e.target.value)}}></input>
         </div>
     );
 }
