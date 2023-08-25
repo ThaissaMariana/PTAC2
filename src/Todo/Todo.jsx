eb50cb514521a51c9b2d561b19954880128a180b
ThaissaMariana <thaissa2608mariana@gmail.com>
2023-08-25 14:29:40 -0400

    Commit alteracao

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -4,29 +4,27 @@ export default function todo () {
 
   const [atividade, setAtividade] = useState("");
   const [lista, setLista] = useState([]);
-
-  const salvar = (e) => {
-    e.preventDefault();
-    setLista([...lista, {
-      atividade: atividade
-    }])
-  }
+  const [id,setId] = useState(1);
+    const salvar =(e) =>{
+        e.preventDefault();
+        setLista([...lista, {
+                atividade: atividade,
+                id: id
+        }]);
+        setId(id + 1);
+        setAtividade("");
+    };
 
   return (
     <div>
       <Link to="/">home</Link>
       <h1>Lista de Atividades</h1>
-
-      <p>{atividade}</p>
-
       <form onSubmit={salvar}>
-
-        <input value={atividade}
+        <input value={atividade} type="text"
              onChange={(e) => setAtividade(e.target.value)} />
 
         <button>ADD</button>
       </form>
-
       {lista.map((ativ)=> <p>{ativ.atividade}</p>)}
 
     </div>
