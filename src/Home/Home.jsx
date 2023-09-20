77cd25526387ccf6a02533eeef1ae7439736cd1d
ThaissaMariana <thaissa2608mariana@gmail.com>
2023-09-20 10:01:52 -0400

    Commit Homemudanca

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,7 +4,8 @@ export default function Home() {
    
     return (
         <div>
-            <h1>Home</h1>
+            <h1 class="home-nome">Produtos Hello Kitty</h1>
+            <img src="https://64.media.tumblr.com/2daa10a683da64c3008f888b41e02402/1552c74e4f9174a7-f9/s1280x1920/e620a7e2d4a29ef4a654e204264ba71c6e2d1c28.gif" alt="" class="imagem-home"/>
             <Link to="/todo">todo</Link>
         </div>
 
